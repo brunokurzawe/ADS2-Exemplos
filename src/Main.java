368b0496c3c79ae02468935ca3360b04c9d5f6d7
brunokurzawe <bruno.kurzawe@gmail.com>
2023-07-11 19:32:34 -0300

    Criado o branch de operadores com exemplos de operações sobre tipos primitivos.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello ADS2!");
+        int A = 15;
+        int B = 20;
+        int C = 12;
+        int RESULTADO = 0;
+
+        RESULTADO = A + B; //SOMA
+        System.out.println(RESULTADO);
+
+        RESULTADO = A - B; //SUBTRACAO
+        System.out.println(RESULTADO);
+
+        RESULTADO = A * B; //MULTIPLICACAO
+        System.out.println(RESULTADO);
+
+        RESULTADO = A / B; //DIVISAO
+        System.out.println(RESULTADO);
+
+        RESULTADO = A % B; //RESTO
+        System.out.println(RESULTADO);
+
     }
 }
