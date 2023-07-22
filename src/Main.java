dc42a5d1ed8731d19db8250aa2f6a61c318a7a70
BETHA\bruno.kurzawe <compras@4>
2023-07-21 22:14:20 -0300

    Exemplo POO -- Classes Wrapper

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello ADS2!");
+        // Wrapper Boolean
+        Boolean boolValue = true;
+        System.out.println("Boolean Value: " + boolValue);
+
+        // Wrapper Byte
+        Byte byteValue = 123;
+        System.out.println("Byte Value: " + byteValue);
+
+        // Wrapper Character
+        Character charValue = 'A';
+        System.out.println("Character Value: " + charValue);
+
+        // Wrapper Double
+        Double doubleValue = 3.14;
+        System.out.println("Double Value: " + doubleValue);
+
+        // Wrapper Float
+        Float floatValue = 2.71f;
+        System.out.println("Float Value: " + floatValue);
+
+        // Wrapper Integer
+        Integer intValue = 42;
+        System.out.println("Integer Value: " + intValue);
+
+        // Wrapper Long
+        Long longValue = 123456789L;
+        System.out.println("Long Value: " + longValue);
+
+        // Classe Math
+        double squareRoot = Math.sqrt(25);
+        System.out.println("Square Root: " + squareRoot);
+
+        // Wrapper Number
+        Number numberValue = 123.45;
+        System.out.println("Number Value: " + numberValue);
+
+        // Wrapper Short
+        Short shortValue = 100;
+        System.out.println("Short Value: " + shortValue);
+
+        // Wrapper Object
+        Object objectValue = "Hello, Java!";
+        System.out.println("Object Value: " + objectValue);
     }
 }
