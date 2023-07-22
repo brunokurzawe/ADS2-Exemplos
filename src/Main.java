06ffce71d6dc9b4f3c6489a737fb87ab04cf0386
BETHA\bruno.kurzawe <compras@4>
2023-07-21 22:44:18 -0300

    Exemplo POO -- Exemplos de tratamento de excecoes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello ADS2!");
+        try {
+            Object valor = "123";
+            int numero = (int) valor;
+            System.out.println("Número: " + numero);
+
+        } catch (ClassCastException e) {
+            System.out.println("Erro: ClassCastException - Conversão inválida.");
+        }
+
+        try {
+            int numerador = 10;
+            int denominador = 0;
+            int resultado = numerador / denominador;
+            System.out.println("Resultado: " + resultado);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Erro: ArithmeticException - Divisão por zero não é permitida.");
+        }
     }
 }
