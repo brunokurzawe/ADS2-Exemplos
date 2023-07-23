f9d18fd85093a29001e5bdf5c3c41e99b2ffd795
BETHA\bruno.kurzawe <compras@4>
2023-07-23 18:08:27 -0300

    Exemplo POO -- polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        AlunoFaculdade aluno = new AlunoFaculdade(10.0, 8.0, 7.0);
+        Aluno aluno = new AlunoFaculdade(10.0, 8.0, 7.0);
         aluno.setNome("Bruno");
 
-        Boletim boletim = new Boletim(aluno);
+        Aluno aluno02 = new AlunoMedio(10.0, 8.0, 7.0);
+        aluno02.setNome("Maria");
+
+        Aluno aluno03 = new AlunoFundamental(10.0, 8.0, 7.0, 5.0);
+        aluno03.setNome("Pedro");
+
+        BoletimPolimorfico boletim = new BoletimPolimorfico(aluno);
         boletim.imprimir();
+
+        BoletimPolimorfico boletim2 = new BoletimPolimorfico(aluno02);
+        boletim2.imprimir();
+
+        BoletimPolimorfico boletim3 = new BoletimPolimorfico(aluno03);
+        boletim3.imprimir();
+
     }
 
 }
